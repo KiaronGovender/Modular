9958f0c0499be3ce07ae0918428ccebc4a963899
KiaronGovender <kiagov02@gmail.com>
2026-02-04 17:31:49 +0200

    removed duplicate footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Footer from "./components/Footer";
 import "./styles/globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,7 +74,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
